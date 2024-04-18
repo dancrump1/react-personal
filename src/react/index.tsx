a5ff684677d126463bc673d3fc8f2a5bc73a60ca
Dan <danielcrump@outlook.com>
2024-04-18 13:59:16 -0400

    change colors

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -136,7 +136,7 @@ function App() {
 
       <h2 className="h-[55px] mt-20 relative z-10 text-5xl bg-clip-text md:text-7xl text-transparent bg-gradient-to-b from-neutral-500 to-neutral-600 text-center font-sans font-bold lg:h-[85px] md:h-[75px]">
         Some of my{" "}
-        <span className="bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+        <span className="bg-clip-text bg-gradient-to-r to-emerald-700 from-sky-600 dark:to-emerald-600 dark:from-sky-400">
           Projects
         </span>
       </h2>
