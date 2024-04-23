8b76529160ba1049e45f9dc75694bd32c204ba2b
Dan <danielcrump@outlook.com>
2024-04-23 14:43:40 -0400

    add party toggle and prof pics

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Moon, Sun } from "lucide-react";
+import React from "react";
+import { Moon, Sun, BriefcaseBusiness, PartyPopper } from "lucide-react";
 
 import { Button } from "./button";
 import {
@@ -11,28 +11,55 @@ import {
 import { useTheme } from "./theme-provider";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, setFun } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <BriefcaseBusiness
+              className={`absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all ${
+                localStorage.getItem("fun-mode") === "business" && "!scale-100"
+              }`}
+            />
+            <PartyPopper
+              className={`h-[1.2rem] w-[1.2rem] scale-100 transition-all ${
+                localStorage.getItem("fun-mode") === "business" && "!scale-0"
+              } `}
+            />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setFun("party")}>
+            Party 🎉
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setFun("business")}>
+            Business 🕴️
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -1,21 +1,28 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 type Theme = "dark" | "light" | "system";
+type Fun = "party" | "business";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
   defaultTheme?: Theme;
+  defaultFunTheme?: Fun;
   storageKey?: string;
+  storageFunKey?: string;
 };
 
 type ThemeProviderState = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
+  fun: Fun;
+  setFun: (fun: Fun) => void;
 };
 
 const initialState: ThemeProviderState = {
   theme: "system",
   setTheme: () => null,
+  fun: "business",
+  setFun: () => null,
 };
 
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
@@ -23,17 +30,22 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 export function ThemeProvider({
   children,
   defaultTheme = "system",
+  defaultFunTheme = "business",
   storageKey = "vite-ui-theme",
+  storageFunKey = "fun-mode",
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
   );
+  const [fun, setFun] = useState<Fun>(
+    () => (localStorage.getItem(storageFunKey) as Fun) || defaultFunTheme
+  );
 
   useEffect(() => {
     const root = window.document.documentElement;
 
-    root.classList.remove("light", "dark");
+    root.classList.remove("light", "dark", "party", "business");
 
     if (theme === "system") {
       const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
@@ -42,17 +54,24 @@ export function ThemeProvider({
         : "light";
 
       root.classList.add(systemTheme);
+      root.classList.add(fun);
       return;
     }
 
     root.classList.add(theme);
-  }, [theme]);
+    root.classList.add(fun);
+  }, [theme, fun]);
 
   const value = {
     theme,
     setTheme: (theme: Theme) => {
       localStorage.setItem(storageKey, theme);
       setTheme(theme);
+    },
+    fun,
+    setFun: (fun: Fun) => {
+      localStorage.setItem(storageFunKey, fun);
+      setFun(fun);
     },
   };
 
